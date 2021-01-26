6cf0b015124620b1b498a7fcdfd68b37b3f94151
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-01-26 23:10:07 +0600

    description formating revised abd vue esitor added

--- back-end/app/Providers/RouteServiceProvider.php
+++ back-end/app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const DASHBOARD = '/home';
     public const VDASHBOARD = '/vendor-dashboard';
 
     /**
