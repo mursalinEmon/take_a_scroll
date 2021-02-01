9dee0a7b2550c8a0c9003e1556af897eff59376b
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-02-01 23:49:41 +0600

    redundant html for calculating discount and coupon removed

--- back-end/resources/views/cart/cart_view.blade.php
+++ back-end/resources/views/cart/cart_view.blade.php
@@ -123,8 +123,6 @@
                                     <h1 class="text text-danger">Empty Cart ....!!</h1>
                                     <br>
                                 @endforelse
-
-
                             </tbody>
                         </table>
                     </div>
@@ -133,7 +131,7 @@
 
                 <div class="row">
 
-                    <div class="col-lg-6 col-12 mb-15">
+                    {{-- <div class="col-lg-6 col-12 mb-15">
                         <!-- Calculate Shipping -->
                         <div class="calculate-shipping">
                             <h4>Calculate Shipping</h4>
@@ -167,20 +165,8 @@
                             </form>
                         </div>
                         <!-- Discount Coupon -->
-                        <div class="discount-coupon">
-                            <h4>Discount Coupon Code</h4>
-                            <form action="#">
-                                <div class="row">
-                                    <div class="col-md-6 col-12 mb-25">
-                                        <input type="text" placeholder="Coupon Code">
-                                    </div>
-                                    <div class="col-md-6 col-12 mb-25">
-                                        <input type="submit" value="Apply Code">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+
+                    </div> --}}
 
                     <!-- Cart Summary -->
                     <div class="col-lg-6 col-12 mb-40 d-flex">
@@ -206,71 +192,7 @@
 </div>
 <!-- Cart Page End -->
 
-<!-- Banner Section Start -->
-<div class="banner-section section mb-90">
-    <div class="container">
-        <div class="row">
 
-            <!-- Banner -->
-            <div class="col-12">
-                <div class="banner"><a href="#"><img src="assets/images/banner/banner-10.jpg" alt="Banner"></a></div>
-            </div>
-
-        </div>
-    </div>
-</div><!-- Banner Section End -->
-
-<!-- Brands Section Start -->
-<div class="brands-section section mb-90">
-    <div class="container">
-        <div class="row">
-
-            <!-- Brand Slider Start -->
-            <div class="brand-slider col">
-                <div class="brand-item col"><img src="assets/images/brands/brand-1.png" alt="Brands"></div>
-                <div class="brand-item col"><img src="assets/images/brands/brand-2.png" alt="Brands"></div>
-                <div class="brand-item col"><img src="assets/images/brands/brand-3.png" alt="Brands"></div>
-                <div class="brand-item col"><img src="assets/images/brands/brand-4.png" alt="Brands"></div>
-                <div class="brand-item col"><img src="assets/images/brands/brand-5.png" alt="Brands"></div>
-            </div><!-- Brand Slider End -->
-
-        </div>
-    </div>
-</div><!-- Brands Section End -->
-
-<!-- Subscribe Section Start -->
-<div class="subscribe-section section bg-gray pt-55 pb-55">
-    <div class="container">
-        <div class="row align-items-center">
-
-            <!-- Mailchimp Subscribe Content Start -->
-            <div class="col-lg-6 col-12 mb-15 mt-15">
-                <div class="subscribe-content">
-                    <h2>SUBSCRIBE <span>OUR NEWSLETTER</span></h2>
-                    <h2><span>TO GET LATEST</span> PRODUCT UPDATE</h2>
-                </div>
-            </div><!-- Mailchimp Subscribe Content End -->
-
-
-            <!-- Mailchimp Subscribe Form Start -->
-            <div class="col-lg-6 col-12 mb-15 mt-15">
-
-				<form class="subscribe-form" action="#">
-					<input type="email" autocomplete="off" placeholder="Enter your email here" />
-					<button >subscribe</button>
-				</form>
-				<!-- mailchimp-alerts Start -->
-				<div class="mailchimp-alerts text-centre">
-					<div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
-					<div class="mailchimp-success"></div><!-- mailchimp-success end -->
-					<div class="mailchimp-error"></div><!-- mailchimp-error end -->
-				</div><!-- mailchimp-alerts end -->
-
-            </div><!-- Mailchimp Subscribe Form End -->
-
-        </div>
-    </div>
-</div><!-- Subscribe Section End -->
 
 <!-- JS
 ============================================ -->
