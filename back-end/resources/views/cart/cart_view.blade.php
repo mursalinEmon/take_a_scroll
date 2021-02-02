1d7a5247fdc4e7a1049443b2845219671717c794
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-02-02 22:53:18 +0600

    modivied cart toatl view

--- back-end/resources/views/cart/cart_view.blade.php
+++ back-end/resources/views/cart/cart_view.blade.php
@@ -116,7 +116,7 @@
                                         <td class="pro-title"><a href="#">{{ $row->name }}</a></td>
                                         <td class="pro-price"><span>${{ $row->price }}</span></td>
                                         <td class="pro-quantity"><div class="pro-qty"><input type="text" value="{{ $row->qty }}"></div></td>
-                                        <td class="pro-subtotal"><span>${{ Cart::subtotal() }}</span></td>
+                                        <td class="pro-subtotal">$<span>{{ $row->price }}</span></td>
                                         <td class="pro-remove"><a href="#"><i class="fa fa-trash-o remove-cart" data-rowid="{{ $row->rowId }}"></i></a></td>
                                     </tr>
                                 @empty
