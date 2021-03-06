60b6cf365f2ae1908786a779223193a60d92d0c9
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-03-06 15:40:52 +0600

    ssl integration partially done

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -133,3 +133,20 @@ View::composer(['*'], function ($view) {
 // dd($shops);
     $view->with(['categories'=>$categories,'cart'=>$cart,'Electronics'=>$electronics_shops,'Realestate'=>$realestate_shops,'Cars'=>$cars_shops,'category_type']);
 });
+
+
+
+
+// SSLCOMMERZ Start
+Route::get('/example1', 'SslCommerzPaymentController@exampleEasyCheckout');
+Route::get('/example2', 'SslCommerzPaymentController@exampleHostedCheckout');
+
+Route::post('/pay', 'SslCommerzPaymentController@index');
+Route::post('/pay-via-ajax', 'SslCommerzPaymentController@payViaAjax');
+
+Route::post('/success', 'SslCommerzPaymentController@success');
+Route::post('/fail', 'SslCommerzPaymentController@fail');
+Route::post('/cancel', 'SslCommerzPaymentController@cancel');
+
+Route::post('/ipn', 'SslCommerzPaymentController@ipn');
+//SSLCOMMERZ END
