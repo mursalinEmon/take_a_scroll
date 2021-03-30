be2066d497aeb9896b8abc65619a3c0b84267692
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-03-30 23:59:38 +0600

    migratin edited

--- back-end/database/migrations/2021_03_20_191623_create_vendors_table.php
+++ back-end/database/migrations/2021_03_20_191623_create_vendors_table.php
@@ -14,7 +14,7 @@ class CreateVendorsTable extends Migration
     public function up()
     {
         Schema::create('vendors', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('name');
