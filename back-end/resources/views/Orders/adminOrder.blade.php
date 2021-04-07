7de1e9a34df9710d50d571d11251abb2ca284257
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-04-08 00:12:12 +0600

    formating almost done but calculation for total money left then procees work

--- back-end/resources/views/Orders/adminOrder.blade.php
+++ back-end/resources/views/Orders/adminOrder.blade.php
@@ -7,33 +7,33 @@
         </div>
         <hr>
         @forelse($store_wise_row as $key=>$value)
-            <h6 class="text text-center text-secondary">Store ID : {{$key}}</h6>
-            @forelse($value as $row)
+            <h2 class="text text-center text-primary">Store ID : {{$key}}</h2>
+
                 <table class="table table-hover">
                     <thead>
                     <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Store Name</th>
-                        <th scope="col">Price</th>
+                        <th class="text text-center" scope="col">Image</th>
+                        <th class="text text-center" scope="col">Name</th>
+                        <th class="text text-center" scope="col">Quantity</th>
+                        <th class="text text-center" scope="col">Store Name</th>
+                        <th class="text text-center" scope="col">Price</th>
                     </tr>
                     </thead>
                     <tbody>
+                    @forelse($value as $row)
                     <tr>
-                        <td scope="row"><img src="{{asset($row->model->product_pictures[0])}}" alt=""></td>
-                        <td>{{$row->model->name}}</td>
-                        <td>{{$row->qty}}</td>
-                        <td>{{$row->model->store->name}}</td>
-
-                        <td>{{$row->model->price * $row->qty}}</td>
+                        <td scope="row"><img style="width: 100px;height: 100px;" src="{{asset($row->model->product_pictures[0])}}" alt=""></td>
+                        <td class="text text-center">{{$row->model->name}}</td>
+                        <td  class="text text-center">{{$row->qty}}</td>
+                        <td  class="text text-center">{{$row->model->store->name}}</td>
+                        <td  class="text text-center">{{$row->model->price * $row->qty}}</td>
                     </tr>
-
+                    @empty
+                        <h1 class="text text-danger">No Data..!!!</h1>
+                    @endforelse
                     </tbody>
                 </table>
-            @empty
-                <h1 class="text text-danger">No Data..!!!</h1>
-            @endforelse
+
         @empty
             <h1 class="text text-danger">No Data..!!!</h1>
         @endforelse
