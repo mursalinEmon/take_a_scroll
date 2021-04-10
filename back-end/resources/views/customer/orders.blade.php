01e58aea99c27a745e2d1fdbd86678164f2d307b
TahzidMahmud <tahzid15-1272@diu.edu.bd>
2021-04-10 09:01:30 +0600

    formating for order info left in customer orders list view

--- back-end/resources/views/customer/orders.blade.php
+++ back-end/resources/views/customer/orders.blade.php
@@ -5,9 +5,32 @@
         @foreach($orders as $order)
             @foreach($carts as $key=>$value)
                 @if($key == $order->id)
+                <table class="table table-hover">
+                    <thead>
+                    <tr>
+                        <th class="text text-center" scope="col">Image</th>
+                        <th class="text text-center" scope="col">Name</th>
+                        <th class="text text-center" scope="col">Quantity</th>
+                        <th class="text text-center" scope="col">Price</th>
+                    </tr>
+                    </thead>
                     @foreach($value as $row)
-                        <p>{{$order->id}} {{$row->model->name}}</p>
+                        {{-- <p>{{$order->id}} {{$row->model->name}}</p> --}}
+
+                            <tbody>
+                                @php $total=0 @endphp
+                                <tr>
+                                    <td scope="row"><img style="width: 100px;height: 100px;" src="{{asset($row->model->product_pictures[0])}}" alt=""></td>
+                                    <td class="text text-center">{{$row->model->name}}</td>
+                                    <td  class="text text-center">{{$order->id}}</td>
+                                    <td  class="text text-center">{{$row->model->price }}</td>
+                                    @php $total+=$row->model->price @endphp
+                                </tr>
+
+                            </tbody>
+
                     @endforeach
+                </table>
 
                 @else
                 @endif
